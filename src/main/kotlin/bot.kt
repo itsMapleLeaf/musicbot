28d4ef46052e0a9d0fcd6c962a6b7cc822af6e96
kingdaro <kingdaro@gmail.com>
2020-06-14 14:22:57 -0500

    more bot code organization

--- src/main/kotlin/bot.kt
+++ src/main/kotlin/bot.kt
@@ -6,7 +6,7 @@ import net.dv8tion.jda.api.events.message.MessageReceivedEvent
 import net.dv8tion.jda.api.hooks.EventListener
 
 private val botToken = System.getenv("BOT_TOKEN") ?: error("BOT_TOKEN env variable not found")
-private const val commandPrefix = ","
+private const val commandPrefix = "-"
 
 private val player = AudioPlayerWrapper()
 
@@ -23,46 +23,47 @@ private val commands = listOf(
     }
 )
 
-private val eventListener = object : EventListener {
-    override fun onEvent(event: GenericEvent) {
-        if (event is ReadyEvent) {
-            println("Ready")
-            event.jda.presence.setPresence(Activity.playing("psytrance"), false)
-        }
+private val Command.prefixText get() = "$commandPrefix$name"
 
-        if (event is MessageReceivedEvent) {
-            fun Command.prefixText() = "$commandPrefix$name"
+private fun handleReady(event: ReadyEvent) {
+    println("Ready")
+    event.jda.presence.setPresence(Activity.playing("psytrance"), false)
+}
 
-            val content = event.message.contentStripped
-            val command = commands.find { content.startsWith(it.prefixText()) }
+private fun handleMessageReceived(event: MessageReceivedEvent) {
+    val content = event.message.contentStripped
+    val command = commands.find { content.startsWith(it.prefixText) }
 
-            if (command != null) {
-                val args = content
-                    .drop(command.prefixText().length)
-                    .split(" ")
-                    .filterNot { it.isBlank() }
-                    .map { it.trim() }
+    if (command != null) {
+        val args = content
+            .drop(command.prefixText.length)
+            .split(" ")
+            .filterNot { it.isBlank() }
+            .map { it.trim() }
 
-                command.run(object : CommandContext {
-                    override val args = args
+        command.run(object : CommandContext {
+            override val args = args
 
-                    override fun reply(message: String) {
-                        event.channel.sendMessage(message)
-                    }
-
-                    override fun joinVoiceChannel(): Boolean {
-                        val voiceState = event.member?.voiceState ?: return false
-                        val channel = voiceState.channel ?: return false
-                        val guild = voiceState.guild
-                        val audioManager = guild.audioManager
-                        audioManager.sendingHandler = player.jdaSendingHandler
-                        audioManager.openAudioConnection(channel)
-                        return true
-                    }
-                })
+            override fun reply(message: String) {
+                event.channel.sendMessage(message)
             }
-        }
+
+            override fun joinVoiceChannel(): Boolean {
+                val voiceState = event.member?.voiceState ?: return false
+                val channel = voiceState.channel ?: return false
+                val guild = voiceState.guild
+                val audioManager = guild.audioManager
+                audioManager.sendingHandler = player.jdaSendingHandler
+                audioManager.openAudioConnection(channel)
+                return true
+            }
+        })
     }
+}
+
+private val eventListener = EventListener { event ->
+    if (event is ReadyEvent) handleReady(event)
+    if (event is MessageReceivedEvent) handleMessageReceived(event)
 }
 
 fun runDiscordBot() {
