c547990bb09eb53f147ba0d2ddc32e3cae42489e
kingdaro <kingdaro@gmail.com>
2020-06-18 09:04:23 -0500

    markdownEscape: handle spoiler characters

--- src/main/kotlin/string.kt
+++ src/main/kotlin/string.kt
@@ -1,4 +1,4 @@
 import org.unbescape.html.HtmlEscape
 
 fun String.markdownEscape(): String =
-    HtmlEscape.unescapeHtml(this).replace(Regex("[_~*]")) { "\\${it.value}" }
+    HtmlEscape.unescapeHtml(this).replace(Regex("([_~*]|\\|\\|)")) { "\\${it.value}" }
